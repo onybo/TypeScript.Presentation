069f7763d9b203e165bb72018c91d89fdfcaca38
on <olavnybo@gmail.com>
2015-05-15 22:42:47 +0200

    adding blink tag directive

---
+++ SampleProject/apps/item/app/BlinkDirective.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../../../scripts/typings/angularjs/angular.d.ts" />
+var Directives;
+(function (Directives) {
+    'use strict';
+
+    blink.$inject = ['$timeout'];
+
+    function blink($timeout) {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {},
+            controller: function ($scope, $element) {
+                function showElement() {
+                    $element.css("background-color", "red");
+                    $timeout(hideElement, 500);
+                }
+
+                function hideElement() {
+                    $element.css("background-color", "yellow");
+                    $timeout(showElement, 500);
+                }
+                showElement();
+            },
+            template: '<span ng-transclude></span>',
+            replace: true
+        };
+    }
+
+    angular.module('app').directive('blink', blink);
+})(Directives || (Directives = {}));
+//# sourceMappingURL=BlinkDirective.js.map
---
+++ SampleProject/apps/item/app/BlinkDirective.ts
@@ -0,0 +1,33 @@
+﻿/// <reference path="../../../scripts/typings/angularjs/angular.d.ts" />
+
+module Directives {
+    'use strict';
+
+    blink.$inject = ['$timeout'];
+
+    function blink($timeout: ng.ITimeoutService): ng.IDirective {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {},
+            controller: function ($scope, $element) {
+                function showElement() {
+                    $element.css("background-color", "red");
+                    $timeout(hideElement, 500);
+                }
+
+                function hideElement() {
+                    $element.css("background-color", "yellow");
+                    $timeout(showElement, 500);
+                }
+                showElement();
+            },
+            template: '<span ng-transclude></span>',
+            replace: true
+        };
+    }
+
+    angular
+        .module('app')
+        .directive('blink', blink);
+}
